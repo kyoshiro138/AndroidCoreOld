e0fe0a3df913fe4fdcbb43a7a4eef92bd52d0b74
kyoshiro138 <kyoshiro138@yahoo.com>
2015-08-21 00:07:32 +0700

    Update appear animator

--- app/src/main/java/com/jp/androidcore/app/animation/AnimationFragment.java
+++ app/src/main/java/com/jp/androidcore/app/animation/AnimationFragment.java
@@ -8,9 +8,11 @@ import android.widget.Toast;
 import com.jp.androidcore.R;
 import com.jp.androidcore.appimplemetation.activity.drawer.AppDrawerActivity;
 import com.jp.androidcore.appimplemetation.fragment.AppFragment;
+import com.jp.androidcore.core.animation.AppearAnimator;
 
 public class AnimationFragment extends AppFragment<AppDrawerActivity> implements View.OnClickListener {
     private View mAnimationView;
+    private AppearAnimator mAppearAnimation;
 
     @Override
     protected int getFragmentLayoutResource() {
@@ -33,39 +35,13 @@ public class AnimationFragment extends AppFragment<AppDrawerActivity> implements
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_animation_appear:
-                startAnimation(mAnimationView);
+                if(mAppearAnimation == null) {
+                    mAppearAnimation = new AppearAnimator(mAnimationView, "APPEAR");
+                }
+                mAppearAnimation.start();
                 break;
             default:
                 break;
         }
-    }
-
-    private void startAnimation(final View view) {
-        final int viewWidth = view.getMeasuredWidth();
-        final int viewHeight = view.getMeasuredHeight();
-        final float viewY = view.getY();
-
-        Toast.makeText(getActivity(), String.format("view %d %d", viewWidth, viewHeight), Toast.LENGTH_SHORT).show();
-
-        ValueAnimator widthAnimation = ValueAnimator.ofInt(0, 100);
-        widthAnimation.setDuration(1000);
-        widthAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                int val = (Integer) valueAnimator.getAnimatedValue();
-                ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
-
-                int width = viewWidth * val / 100;
-                int height = viewHeight * val / 100;
-                float y = viewY + ((viewHeight - height) / 2);
-
-                layoutParams.width = width;
-                layoutParams.height = height;
-
-                view.setLayoutParams(layoutParams);
-                view.setY(y);
-            }
-        });
-        widthAnimation.start();
     }
 }
---
+++ app/src/main/java/com/jp/androidcore/core/animation/BaseAnimator.java
@@ -0,0 +1,59 @@
+package com.jp.androidcore.core.animation;
+
+import android.animation.Animator;
+import android.animation.TimeInterpolator;
+import android.view.View;
+
+public abstract class BaseAnimator implements Animator.AnimatorListener {
+    protected View mAnimationView;
+    protected String mTag;
+    private Animator mAnimator;
+
+    public BaseAnimator(View view, String tag) {
+        mAnimationView = view;
+        mTag = tag;
+
+        mAnimator = initAnimation(mAnimationView);
+        mAnimator.addListener(this);
+    }
+
+    protected abstract Animator initAnimation(View view);
+
+    public void start() {
+        if (mAnimator != null && !mAnimator.isRunning()) {
+            mAnimator.start();
+        }
+    }
+
+    public void setDuration(long milliseconds) {
+        if (mAnimator != null) {
+            mAnimator.setDuration(milliseconds);
+        }
+    }
+
+    public void setInterpolator(TimeInterpolator interpolator) {
+        if (mAnimator != null) {
+            mAnimator.setInterpolator(interpolator);
+        }
+    }
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+}
